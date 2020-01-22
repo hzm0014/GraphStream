a51c9ca171979a4cadfca38720fb736d88f5b04e
hzm0014 <hzm0014@hzm-mb.local>
2020-01-23 02:42:36 +0900

    first commit

---
+++ GenerateGraph.java
@@ -0,0 +1,26 @@
+import org.graphstream.graph.*;
+import org.graphstream.graph.implementations.*;
+
+public class GenerateGraph extends Thread {
+	public static void main(String args[]) {
+		Graph graph = new SingleGraph("Graph");
+
+    graph.setStrict(false);
+    graph.setAutoCreate( true );
+		graph.display();
+
+    graph.addNode("A");
+    for (int i = 1; i < 100; i++) {
+      try {
+        graph.addEdge(
+          "A" + i,
+          "A",
+          i + ""
+        );
+        Thread.sleep(100);
+      } catch (InterruptedException e) {
+          e.printStackTrace();
+      }
+    }
+	}
+}
